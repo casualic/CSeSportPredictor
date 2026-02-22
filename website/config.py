288b5a59ed40b45fdca0e173abf31103724e1f49
casualic <mat.delpercio@gmail.com>
2026-02-22 11:31:15 +0100

    fixed version

--- website/config.py
+++ website/config.py
@@ -18,20 +18,25 @@ PLAYER_WINDOW = 10
 MAP_WINDOW = 20
 
 # 47 lean features in exact training order
+# IMPORTANT: This order must match the column order from build_features() in
+# train_model_fsvm.py, since the scaler and FSVM model were trained with this order.
 LEAN_COLS = [
+    "elo_diff", "rank_diff", "rank_ratio", "log_rank_ratio",
+    "dyn_rank_diff", "dyn_log_rank_ratio",
+    "elo_rank_diff", "log_elo_rank_ratio",
+    "form_diff", "streak_diff", "h2h_winrate", "h2h_matches",
+    "elo_expected", "momentum_diff", "vs_strong_diff",
+    "map_pool_depth_diff", "map_wr_overlap",
+    "rank_vol_diff", "rank_vol_max", "rank_trajectory_diff",
+    "rank_confidence", "rank_conf_x_rank_diff",
     "diff_avg_rating", "diff_avg_adr", "diff_avg_kast", "diff_avg_kd_diff",
     "diff_star_rating", "diff_weakest_rating", "diff_star_gap", "diff_consistency",
     "diff_avg_rest", "diff_roster_exp", "diff_chemistry",
-    "elo_diff", "elo_expected", "rank_diff", "log_rank_ratio", "rank_ratio",
-    "dyn_rank_diff", "dyn_log_rank_ratio",
-    "elo_rank_diff", "log_elo_rank_ratio", "momentum_diff", "vs_strong_diff",
-    "form_diff", "streak_diff", "h2h_winrate", "h2h_matches", "home_diff",
+    "home_diff",
     "is_bo1", "is_bo3", "is_bo5",
     "rank_diff_x_bo", "dyn_rank_diff_x_bo", "elo_rank_diff_x_bo",
     "elo_diff_x_form", "rank_x_h2h", "momentum_x_form",
-    "map_pool_depth_diff", "map_wr_overlap", "map_upset_potential",
-    "rank_vol_diff", "rank_vol_max", "rank_trajectory_diff",
-    "rank_confidence", "rank_conf_x_rank_diff",
+    "map_upset_potential",
     "upset_prob", "upset_prob_x_rank_diff",
     "pistol_wr_diff",
 ]
